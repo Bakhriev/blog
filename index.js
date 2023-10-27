8d77b9a54b1672aaa1ec24945aaf48ab0d16ba29
Bakhriev <legendofpain5112@gmail.com>
2023-10-27 17:55:24 +0500

    rew

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ import { handleLogin } from "./routes/auth/login.js"
 import { handleRegister } from "./routes/auth/register.js"
 
 dotenv.config()
+
 const app = express()
+
 app.use(express.json())
 
 mongoose
@@ -18,8 +20,6 @@ mongoose
 	})
 	.catch(error => console.log(`Ошибка при подключении к базе данных: ${error}`))
 
-// Обработчики маршрутов ---------------------------------------------------------
-
 app.get("/", (req, res) => {
 	res.send("h1")
 })
@@ -27,8 +27,6 @@ app.get("/", (req, res) => {
 app.post("/auth/login", handleLogin)
 
 app.post("/auth/register", registerValidation, handleRegister)
-
-// Поднятие сервера ----------------------------------------------------------------
 
 app.listen(process.env.PORT || 4444, () => {
 	console.log(`Сервер успешно запущен на port: ${process.env.PORT || 4444}`)
